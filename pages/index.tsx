e1649f4dbc4e696ccb5a667df5b5bd2f80e14d08
PBJT <50968378+PBJT2@users.noreply.github.com>
2022-05-21 07:01:14 -0700

    Dynamic Host Name on Home Page example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ host }) => {
+  if (typeof window !== "undefined") {
+    const hostname = window.location.hostname;
+  }
   return (
     <>
       <Head>
@@ -12,28 +15,43 @@ const Home: NextPage = () => {
       </Head>
       <div className="h-screen bg-black grid items-center justify-center">
         <div className="border-white border-2 max-w-lg rounded-lg px-2 py-1 text-white">
-          <h1 className="text-center text-xl font-bold">
-            PBJT Multi Twitch Player
-          </h1>
-          <p className="text-justify text-xs whitespace-pre-wrap">
-            Welcome to the PBJT Multi Twitch Player! You can use this site to
-            watch any number of&nbsp;
-            <a href="" className="text-blue-500 cursor-pointer hover:underline">
-              twitch.tv
-            </a>
-            &nbsp;streams at the same time (as long as your computer can handle
-            it). Simply put the streams you want in the url. For example:&nbsp;
-            <Link href="/pbjt__/chlocakes">
-              <a className="text-blue-500 cursor-pointer hover:underline">
-                localhost:3000/pbjt__/chlocakes
+          <main>
+            <h1 className="text-center text-xl font-bold">
+              PBJT Multi Twitch Player
+            </h1>
+            <p className="text-justify text-xs whitespace-pre-wrap">
+              Welcome to the PBJT Multi Twitch Player! You can use this site to
+              watch any number of&nbsp;
+              <a
+                href=""
+                className="text-blue-500 cursor-pointer hover:underline"
+              >
+                twitch.tv
               </a>
-            </Link>
-            . Happy streamwatching!
-          </p>
+              &nbsp;streams at the same time (as long as your computer can
+              handle it). Simply put the streams you want in the url. For
+              example:&nbsp;
+              <Link href="/pbjt__/chlocakes">
+                <a className="text-blue-500 cursor-pointer hover:underline">
+                  {host}/pbjt__/chlocakes
+                </a>
+              </Link>
+              . Happy streamwatching!
+            </p>
+          </main>
         </div>
       </div>
     </>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { req } = context;
+  let host;
+  if (req) {
+    host = req.headers.host;
+  }
+  return { props: { host } };
 };
 
 export default Home;
